06d87b8e6e1efc748f8d194a9ce3734ec1170e69
GHOUFA <zohayeramine@gmail.com>
2019-12-20 10:46:08 +0100

    first commit

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+/**
+ * Created by PhpStorm.
+ * User: GHOUFA Zouhayer Amine
+ * Date: 19/12/19
+ * Time: 12:52
+ */
+class MovieController extends ApiController
+{
+    /**
+     * @Route("/movies", methods="GET")
+     */
+    public function index(MovieRepository $movieRepository)
+    {
+        if (! $this->isAuthorized()) {
+            return $this->respondUnauthorized();
+        }
+        $movies = $movieRepository->transformAll();
+
+        return $this->respond($movies);
+    }
+    /**
+     * @Route("/movies", methods="POST")
+     */
+    public function create(Request $request, MovieRepository $movieRepository, EntityManagerInterface $em)
+    {
+        if (! $this->isAuthorized()) {
+            return $this->respondUnauthorized();
+        }
+
+        $request = $this->transformJsonBody($request);
+
+        if (! $request) {
+            return $this->respondValidationError('Please provide a valid request!');
+        }
+
+        // validate the title
+        if (! $request->get('title')) {
+            return $this->respondValidationError('Please provide a title!');
+        }
+
+        // persist the new movie
+        $movie = new Movie;
+        $movie->setTitle($request->get('title'));
+        $movie->setCount(0);
+        $em->persist($movie);
+        $em->flush();
+
+        return $this->respondCreated($movieRepository->transform($movie));
+    }
+
+    /**
+     * @Route("/movies/{id}/count", methods="POST")
+     */
+    public function increaseCount($id, EntityManagerInterface $em, MovieRepository $movieRepository)
+    {
+        if (! $this->isAuthorized()) {
+            return $this->respondUnauthorized();
+        }
+        
+        $movie = $movieRepository->find($id);
+
+        if (! $movie) {
+            return $this->respondNotFound();
+        }
+
+        $movie->setCount($movie->getCount() + 1);
+        $em->persist($movie);
+        $em->flush();
+
+        return $this->respond([
+            'count' => $movie->getCount()
+        ]);
+    }
+
+}
